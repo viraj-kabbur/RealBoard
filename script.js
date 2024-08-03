fa70183e86aae1501314fd4a5eb9f4d4a99c2315
Viraj Kabbur <142905222+viraj-kabbur@users.noreply.github.com>
2024-08-04 04:48:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,195 +1,86 @@
 function initAnimationsLarge() {
 
-var tl = gsap.timeline()
-
-tl.from(".container .box1", {
-    opacity:0,
-    delay:1,
-    duration:0.60
-})
-
-tl.from(".container .box2", {
-    opacity:0,
-    duration:0.60
-})
-tl.from(".container .box3", {
-    opacity:0,
-    duration:0.60
-
-})
-tl.from(".container .box4", {
-    opacity:0,
-    duration:0.60
-})
-tl.from(".container .box5", {
-    opacity:0,
-    duration:0.60
-})
-tl.from(".container .box6", {
-    opacity:0,
-    duration:0.60
-})
-tl.from(".container .box7", {
-    opacity:0,
-    duration:0.60
-})
-tl.from(".container .box8", {
-    opacity:0,
-    duration:0.60
-})
-
-tl.from(".container .box9", {
-    opacity:0,
-    duration:0.60
-})
-
-tl.from(".container .box10", {
-    opacity:0,
-    duration:0.60
-})
-
-tl.from(".container .box11", {
-    opacity:0,
-    duration:0.60
-})
-
-tl.from(".container .box12", {
-    opacity:0,
-    duration:0.60
-})
-tl.from(".container .box13", {
-    opacity:0,
-    duration:0.60
-})
-
-tl.from(".container .box14", {
-    opacity:0,
-    duration:0.60
-})
-
-tl.to(".container", {
-    scale:0.5,
-    duration:1,
-    x:-284,
-    y:-30,
-    scrub:2
-})
-
-tl.from(".banner", {
-    opacity:0,
-    duration:1,
-})
-
-tl.from(".text1", {
-    opacity:0,
-    duration:2,
-    Y:-70,
-    scrub:5
-})
-
-gsap.from(".text2", {
-    opacity:0,
-    duration:0.75,
-    delay:13
-})
-
-gsap.from(".text3", {
-    opacity:0,
-    duration:0.75,
-    delay:13
-})
-
-gsap.from(".button-container", {
-    opacity:0,
-    duration:1,
-    delay:13
-});
-}
-
-
-function initAnimationsSmall() {
-
-    var tl_1 = gsap.timeline()
+    var tl = gsap.timeline()
     
-    tl_1.from(".container .box1", {
+    tl.from(".container .box1", {
         opacity:0,
         delay:1,
         duration:0.60
     })
     
-    tl_1.from(".container .box2", {
+    tl.from(".container .box2", {
         opacity:0,
         duration:0.60
     })
-    tl_1.from(".container .box3", {
+    tl.from(".container .box3", {
         opacity:0,
         duration:0.60
     
     })
-    tl_1.from(".container .box4", {
+    tl.from(".container .box4", {
         opacity:0,
         duration:0.60
     })
-    tl_1.from(".container .box5", {
+    tl.from(".container .box5", {
         opacity:0,
         duration:0.60
     })
-    tl_1.from(".container .box6", {
+    tl.from(".container .box6", {
         opacity:0,
         duration:0.60
     })
-    tl_1.from(".container .box7", {
+    tl.from(".container .box7", {
         opacity:0,
         duration:0.60
     })
-    tl_1.from(".container .box8", {
-        opacity:0,
-        duration:0.60
-    })
-    
-    tl_1.from(".container .box9", {
+    tl.from(".container .box8", {
         opacity:0,
         duration:0.60
     })
     
-    tl_1.from(".container .box10", {
+    tl.from(".container .box9", {
         opacity:0,
         duration:0.60
     })
     
-    tl_1.from(".container .box11", {
+    tl.from(".container .box10", {
         opacity:0,
         duration:0.60
     })
     
-    tl_1.from(".container .box12", {
-        opacity:0,
-        duration:0.60
-    })
-    tl_1.from(".container .box13", {
+    tl.from(".container .box11", {
         opacity:0,
         duration:0.60
     })
     
-    tl_1.from(".container .box14", {
+    tl.from(".container .box12", {
         opacity:0,
         duration:0.60
     })
-        
-    tl_1.to(".container", {
-        scale:0.3,
+    tl.from(".container .box13", {
+        opacity:0,
+        duration:0.60
+    })
+    
+    tl.from(".container .box14", {
+        opacity:0,
+        duration:0.60
+    })
+    
+    tl.to(".container", {
+        scale:0.5,
         duration:1,
-        x:-250,
-        y:-115,
+        x:-284,
+        y:-30,
         scrub:2
     })
     
-    tl_1.from(".banner", {
+    tl.from(".banner", {
         opacity:0,
         duration:1,
     })
     
-    tl_1.from(".text1", {
+    tl.from(".text1", {
         opacity:0,
         duration:2,
         Y:-70,
@@ -207,27 +98,136 @@ function initAnimationsSmall() {
         duration:0.75,
         delay:13
     })
-
-    gsap.from(".text4", {
-        opacity:0,
-        duration:0.75,
-        delay:13
-    })
     
     gsap.from(".button-container", {
         opacity:0,
         duration:1,
         delay:13
     });
-  
-}
-
-let mm = gsap.matchMedia();
-
-mm.add("(min-width: 1025px)", () => {
-    initAnimationsLarge();
-});
-
-mm.add("(min-width: 768px) and (max-width: 1024px)", () => {
-    initAnimationsSmall();
-});
+    }
+    
+    
+    function initAnimationsSmall() {
+    
+        var tl_1 = gsap.timeline()
+        
+        tl_1.from(".container .box1", {
+            opacity:0,
+            delay:1,
+            duration:0.60
+        })
+        
+        tl_1.from(".container .box2", {
+            opacity:0,
+            duration:0.60
+        })
+        tl_1.from(".container .box3", {
+            opacity:0,
+            duration:0.60
+        
+        })
+        tl_1.from(".container .box4", {
+            opacity:0,
+            duration:0.60
+        })
+        tl_1.from(".container .box5", {
+            opacity:0,
+            duration:0.60
+        })
+        tl_1.from(".container .box6", {
+            opacity:0,
+            duration:0.60
+        })
+        tl_1.from(".container .box7", {
+            opacity:0,
+            duration:0.60
+        })
+        tl_1.from(".container .box8", {
+            opacity:0,
+            duration:0.60
+        })
+        
+        tl_1.from(".container .box9", {
+            opacity:0,
+            duration:0.60
+        })
+        
+        tl_1.from(".container .box10", {
+            opacity:0,
+            duration:0.60
+        })
+        
+        tl_1.from(".container .box11", {
+            opacity:0,
+            duration:0.60
+        })
+        
+        tl_1.from(".container .box12", {
+            opacity:0,
+            duration:0.60
+        })
+        tl_1.from(".container .box13", {
+            opacity:0,
+            duration:0.60
+        })
+        
+        tl_1.from(".container .box14", {
+            opacity:0,
+            duration:0.60
+        })
+            
+        tl_1.to(".container", {
+            scale:0.3,
+            duration:1,
+            x:-250,
+            y:-115,
+            scrub:2
+        })
+        
+        tl_1.from(".banner", {
+            opacity:0,
+            duration:1,
+        })
+        
+        tl_1.from(".text1", {
+            opacity:0,
+            duration:2,
+            Y:-70,
+            scrub:5
+        })
+        
+        gsap.from(".text2", {
+            opacity:0,
+            duration:0.75,
+            delay:13
+        })
+        
+        gsap.from(".text3", {
+            opacity:0,
+            duration:0.75,
+            delay:13
+        })
+    
+        gsap.from(".text4", {
+            opacity:0,
+            duration:0.75,
+            delay:13
+        })
+        
+        gsap.from(".button-container", {
+            opacity:0,
+            duration:1,
+            delay:13
+        });
+      
+    }
+    
+    let mm = gsap.matchMedia();
+    
+    mm.add("(min-width: 1024px)", () => {
+        initAnimationsLarge();
+    });
+    
+    mm.add("(min-width: 768px) and (max-width: 1023px)", () => {
+        initAnimationsSmall();
+    });
