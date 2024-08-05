d1f2f0036d5106969a4d56f5fe10b7783c3b6bc2
Viraj Kabbur <142905222+viraj-kabbur@users.noreply.github.com>
2024-08-05 21:01:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,153 @@
-function  function_1201px() {
+function function_1280px() {
+
+    console.log("1280");
+
+    var tl = gsap.timeline()
+    
+// tl.from(".grid-container .one", {
+//     opacity:0,
+//     duration:0.60
+// })
+
+// tl.from(".grid-container .two", {
+//     opacity:0,
+//     duration:0.60
+// })
+
+
+// tl.from(".grid-container .three", {
+//     opacity:0,
+//     duration:0.60
+// })
+
+// tl.from(".grid-container .four", {
+//     opacity:0,
+
+//     duration:0.60
+// })
+
+
+// tl.from(".grid-container .five", {
+//     opacity:0,
+
+//     duration:0.60
+// })
+
+
+// tl.from(".grid-container .six", {
+//     opacity:0,
+
+//     duration:0.60
+// })
+
+
+// tl.from(".grid-container .seven", {
+//     opacity:0,
+
+//     duration:0.60
+// })
+
+// tl.from(".grid-container .eight", {
+//     opacity:0,
+
+//     duration:0.60
+// })
+
+// tl.from(".grid-container .nine", {
+//     opacity:0,
+
+//     duration:0.60
+// })
+
+
+// tl.from(".grid-container .ten", {
+//     opacity:0,
+
+//     duration:0.60
+// })
+
+// tl.from(".grid-container .eleven", {
+//     opacity:0,
+
+//     duration:0.60
+// })
+
+// tl.from(".grid-container .twelve", {
+//     opacity:0,
+
+//     duration:0.60
+// })
+
+// tl.from(".grid-container .thirteen", {
+//     opacity:0,
+
+//     duration:0.60
+// })
+
+// tl.from(".grid-container .fourteen", {
+//     opacity:0,
+
+//     duration:0.60
+// })
+
+// tl.from(".grid-container .fifteen", {
+//     opacity:0,
+
+//     duration:0.60
+// })
+
+// tl.from(".grid-container .sixteen", {
+//     opacity:0,
+
+//     duration:0.60
+// })
+
+tl.to(".grid-container ", {
+    scale:0.45,
+    duration:1,
+    x:-300,
+    y:-90
+})
+    
+    tl.from(".banner", {
+        opacity:0,
+        duration:1,
+    })
+    
+    tl.from(".text1", {
+        opacity:0,
+        duration:1,
+        scrub:5
+    })
+    
+    gsap.from(".text2", {
+        opacity:0,
+        duration:0.75,
+        // delay:13
+    })
+    
+    gsap.from(".text3", {
+        opacity:0,
+        duration:0.75,
+        // delay:13
+    })
+    
+    gsap.from(".button-container", {
+        opacity:0,
+        duration:1,
+        // delay:13
+    });
+}
+
+
+
+
+
+
+function function_1199px() {
+
+    console.log("1199");
+
     var tl = gsap.timeline()
     
 tl.from(".grid-container .one", {
@@ -95,14 +244,15 @@ tl.from(".grid-container .fifteen", {
 
 tl.from(".grid-container .sixteen", {
     opacity:0,
+
     duration:0.60
 })
 
 tl.to(".grid-container ", {
     scale:0.4,
     duration:1,
-    x:-298,
-    y:-90
+    x:-310,
+    y:-100
 })
     
     tl.from(".banner", {
@@ -135,143 +285,290 @@ tl.to(".grid-container ", {
     });
 }
 
-function initAnimationsMedium() {
-    var tl = gsap.timeline()
 
-    tl.from(".grid-container .one", {
+
+function function_1180px() {
+
+    console.log("1180");
+
+    var tl = gsap.timeline()
+    
+tl.from(".grid-container .one", {
+    opacity:0,
+    duration:0.60
+})
+
+tl.from(".grid-container .two", {
+    opacity:0,
+    duration:0.60
+})
+
+
+tl.from(".grid-container .three", {
+    opacity:0,
+    duration:0.60
+})
+
+tl.from(".grid-container .four", {
+    opacity:0,
+
+    duration:0.60
+})
+
+
+tl.from(".grid-container .five", {
+    opacity:0,
+
+    duration:0.60
+})
+
+
+tl.from(".grid-container .six", {
+    opacity:0,
+
+    duration:0.60
+})
+
+
+tl.from(".grid-container .seven", {
+    opacity:0,
+
+    duration:0.60
+})
+
+tl.from(".grid-container .eight", {
+    opacity:0,
+
+    duration:0.60
+})
+
+tl.from(".grid-container .nine", {
+    opacity:0,
+
+    duration:0.60
+})
+
+
+tl.from(".grid-container .ten", {
+    opacity:0,
+
+    duration:0.60
+})
+
+tl.from(".grid-container .eleven", {
+    opacity:0,
+
+    duration:0.60
+})
+
+tl.from(".grid-container .twelve", {
+    opacity:0,
+
+    duration:0.60
+})
+
+tl.from(".grid-container .thirteen", {
+    opacity:0,
+
+    duration:0.60
+})
+
+tl.from(".grid-container .fourteen", {
+    opacity:0,
+
+    duration:0.60
+})
+
+tl.from(".grid-container .fifteen", {
+    opacity:0,
+
+    duration:0.60
+})
+
+tl.from(".grid-container .sixteen", {
+    opacity:0,
+
+    duration:0.60
+})
+
+tl.to(".grid-container ", {
+    scale:0.4,
+    duration:1,
+    x:-305,
+    y:-100
+})
+    
+    tl.from(".banner", {
         opacity:0,
-        duration:0.60
-    })
-    
-    tl.from(".grid-container .two", {
-        opacity:0,
-        duration:0.60
-    })
-    
-    
-    tl.from(".grid-container .three", {
-        opacity:0,
-        duration:0.60
-    })
-    
-    tl.from(".grid-container .four", {
-        opacity:0,
-    
-        duration:0.60
-    })
-    
-    
-    tl.from(".grid-container .five", {
-        opacity:0,
-    
-        duration:0.60
-    })
-    
-    
-    tl.from(".grid-container .six", {
-        opacity:0,
-    
-        duration:0.60
-    })
-    
-    
-    tl.from(".grid-container .seven", {
-        opacity:0,
-    
-        duration:0.60
-    })
-    
-    tl.from(".grid-container .eight", {
-        opacity:0,
-    
-        duration:0.60
-    })
-    
-    tl.from(".grid-container .nine", {
-        opacity:0,
-    
-        duration:0.60
-    })
-    
-    
-    tl.from(".grid-container .ten", {
-        opacity:0,
-    
-        duration:0.60
-    })
-    
-    tl.from(".grid-container .eleven", {
-        opacity:0,
-    
-        duration:0.60
-    })
-    
-    tl.from(".grid-container .twelve", {
-        opacity:0,
-    
-        duration:0.60
-    })
-    
-    tl.from(".grid-container .thirteen", {
-        opacity:0,
-    
-        duration:0.60
-    })
-    
-    tl.from(".grid-container .fourteen", {
-        opacity:0,
-    
-        duration:0.60
-    })
-    
-    tl.from(".grid-container .fifteen", {
-        opacity:0,
-    
-        duration:0.60
-    })
-    
-    tl.from(".grid-container .sixteen", {
-        opacity:0,
-        duration:0.60
-    })
-    
-    tl.to(".grid-container ", {
-        scale:0.4,
         duration:1,
-        x:-200,
-        y:-70
     })
-        
-        tl.from(".banner", {
-            opacity:0,
-            duration:1,
-        })
-        
-        tl.from(".text1", {
-            opacity:0,
-            duration:1,
-            scrub:5
-        })
-        
-        gsap.from(".text2", {
-            opacity:0,
-            duration:0.75,
-            delay:13
-        })
-        
-        gsap.from(".text3", {
-            opacity:0,
-            duration:0.75,
-            delay:13
-        })
-        
-        gsap.from(".button-container", {
-            opacity:0,
-            duration:1,
-            delay:13
-        });
+    
+    tl.from(".text1", {
+        opacity:0,
+        duration:1,
+        scrub:5
+    })
+    
+    gsap.from(".text2", {
+        opacity:0,
+        duration:0.75,
+        delay:13
+    })
+    
+    gsap.from(".text3", {
+        opacity:0,
+        duration:0.75,
+        delay:13
+    })
+    
+    gsap.from(".button-container", {
+        opacity:0,
+        duration:1,
+        delay:13
+    });
 }
 
+
+function function_1104px() {
+
+    console.log("1104");
+
+    var tl = gsap.timeline()
+    
+tl.from(".grid-container .one", {
+    opacity:0,
+    duration:0.60
+})
+
+tl.from(".grid-container .two", {
+    opacity:0,
+    duration:0.60
+})
+
+
+tl.from(".grid-container .three", {
+    opacity:0,
+    duration:0.60
+})
+
+tl.from(".grid-container .four", {
+    opacity:0,
+
+    duration:0.60
+})
+
+
+tl.from(".grid-container .five", {
+    opacity:0,
+
+    duration:0.60
+})
+
+
+tl.from(".grid-container .six", {
+    opacity:0,
+
+    duration:0.60
+})
+
+
+tl.from(".grid-container .seven", {
+    opacity:0,
+
+    duration:0.60
+})
+
+tl.from(".grid-container .eight", {
+    opacity:0,
+
+    duration:0.60
+})
+
+tl.from(".grid-container .nine", {
+    opacity:0,
+
+    duration:0.60
+})
+
+
+tl.from(".grid-container .ten", {
+    opacity:0,
+
+    duration:0.60
+})
+
+tl.from(".grid-container .eleven", {
+    opacity:0,
+
+    duration:0.60
+})
+
+tl.from(".grid-container .twelve", {
+    opacity:0,
+
+    duration:0.60
+})
+
+tl.from(".grid-container .thirteen", {
+    opacity:0,
+
+    duration:0.60
+})
+
+tl.from(".grid-container .fourteen", {
+    opacity:0,
+
+    duration:0.60
+})
+
+tl.from(".grid-container .fifteen", {
+    opacity:0,
+
+    duration:0.60
+})
+
+tl.from(".grid-container .sixteen", {
+    opacity:0,
+
+    duration:0.60
+})
+
+tl.to(".grid-container ", {
+    scale:0.4,
+    duration:1,
+    x:-285,
+    y:-100
+})
+    
+    tl.from(".banner", {
+        opacity:0,
+        duration:1,
+    })
+    
+    tl.from(".text1", {
+        opacity:0,
+        duration:1,
+        scrub:5
+    })
+    
+    gsap.from(".text2", {
+        opacity:0,
+        duration:0.75,
+        delay:13
+    })
+    
+    gsap.from(".text3", {
+        opacity:0,
+        duration:0.75,
+        delay:13
+    })
+    
+    gsap.from(".button-container", {
+        opacity:0,
+        duration:1,
+        delay:13
+    });
+}
 
 function function_1024px() {
 
@@ -695,23 +992,27 @@ tl.to(".grid-container ", {
 
 let mm = gsap.matchMedia();
 
-// mm.add("(min-width: 1201px)", () => {
-//     function_1201px();
-// });
+mm.add("(min-width: 1202px) and (max-width: 1280px)", () => {
+    function_1280px();
+});
 
-// mm.add("(min-width: 768px) and (max-width: 1023px)", () => {
-//     initAnimationsMedium();
-// });
+mm.add("(min-width: 1181px) and (max-width: 1201px)", () => {
+    function_1199px();
+});
 
-// mm.add("(min-width: 1024px) and (max-width: 1200px)", () => {
-//     ipad();
-// });
+mm.add("(min-width: 1105px) and (max-width: 1180px)", () => {
+    function_1180px();
+});
 
-mm.add("(min-width: 1000px) and (max-width: 1025px)", () => {
+mm.add("(min-width: 1025px) and (max-width: 1104px)", () => {
+    function_1104px();
+});
+
+mm.add("(min-width: 999px) and (max-width: 1024px)", () => {
     function_1024px();
 });
 
-mm.add("(min-width: 769px) and (max-width: 999px)", () => {
+mm.add("(min-width: 769px) and (max-width: 998px)", () => {
     function_998px();
 });
 
